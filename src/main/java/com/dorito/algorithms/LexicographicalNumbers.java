7ebe5b7ea23049b65b4999b5d121b2624ee5b4fc
Dorito <bori12370@gmail.com>
2025-06-08 17:33:48 +0900

    solution & testing

--- src/main/java/com/dorito/algorithms/LexicographicalNumbers.java
+++ src/main/java/com/dorito/algorithms/LexicographicalNumbers.java
@@ -1,4 +1,24 @@
 package com.dorito.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LexicographicalNumbers {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            dfs(i, n, result);
+        }
+        return result;
+    }
+
+    private void dfs(int current, int n, List<Integer> result) {
+        if (current > n) {
+            return;
+        }
+        result.add(current);
+        for (int i = 0; i <= 9; i++) {
+            dfs(current * 10 + i, n, result);
+        }
+    }
 }
