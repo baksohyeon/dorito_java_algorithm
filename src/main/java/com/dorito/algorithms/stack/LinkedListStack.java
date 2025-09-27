9473a050d46f383652c242c95066cf05aee68703
Dorito <bori12370@gmail.com>
2025-09-27 14:38:20 +0900

    linked list push 구현

--- src/main/java/com/dorito/algorithms/stack/LinkedListStack.java
+++ src/main/java/com/dorito/algorithms/stack/LinkedListStack.java
@@ -1,7 +1,8 @@
 package com.dorito.algorithms.stack;
 
+
 public class LinkedListStack<E> implements Stack<E> {
-    private Node<E> top;
+    private Node<E> topNode;
     private final E ERROR = null; // 임의의 에러 값
 
 
@@ -11,11 +12,11 @@ public class LinkedListStack<E> implements Stack<E> {
 
 
     /**
-     * @param newItem
+     * @param newItem 스택에 원소 x 삽입
      */
     @Override
     public void push(E newItem) {
-
+        topNode = new Node<>(newItem, topNode);
     }
 
     /**
@@ -56,5 +57,31 @@ public class LinkedListStack<E> implements Stack<E> {
     @Override
     public void clear() {
 
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        private E getData() {
+            return data;
+        }
+
+        private void setData(E data) {
+            this.data = data;
+        }
+
+        private Node<E> getNext() {
+            return next;
+        }
+
+        private void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
